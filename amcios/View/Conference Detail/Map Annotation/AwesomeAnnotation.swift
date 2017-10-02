2ce4c70c0deeeaed2f53f094dbfd7013d028d645
Matteo Crippa <matteo@boostco.de>
2017-10-02 14:31:36 +0200

    custom annotation;

---
+++ amcios/View/Conference Detail/Map Annotation/AwesomeAnnotation.swift
@@ -0,0 +1,13 @@
+//
+//  AwesomeAnnotation.swift
+//  amcios
+//
+//  Created by Matteo Crippa on 02/10/2017.
+//  Copyright © 2017 Matteo Crippa. All rights reserved.
+//
+import MapKit
+import UIKit
+
+class AwesomeAnnotation: MKPointAnnotation {
+    var pinImage: String!
+}
