6b5b22855fc663831fda6e4d1ed09c6391d2b6e9
Matteo Crippa <matteo@boostco.de>
2017-10-05 18:17:58 +0200

    updated color

--- amcios/View/Extension/UIColor+Extension.swift
+++ amcios/View/Extension/UIColor+Extension.swift
@@ -10,6 +10,6 @@ import UIKit
 
 extension UIColor {
     class var awesomeColor: UIColor {
-        return UIColor(red: 63/255.0, green: 81/255.0, blue: 181/255.0, alpha: 1.0)
+        return UIColor(red: 87/255.0, green: 25/255.0, blue: 184/255.0, alpha: 1.0)
     }
 }
